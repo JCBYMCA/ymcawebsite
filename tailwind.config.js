2e8e148ce1ab1bbb86063a7e3245fb55bf80bb29
Mister-H <hs913271@gmail.com>
2022-09-30 17:25:13 +0530

    config files added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#640100",
+        "secondary": "#173C81",
+        "gradient-from":'rgba(0,0,0,0.50)',
+      }
+    },
+  },
+  plugins: [],
+}
