00423f3f456464f9c30159379f9be022f5f94637
Komal <kkomalbhatia15@gmail.com>
2022-10-02 15:16:18 +0530

    -Cards added

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,8 +4,8 @@ import {useTranslations} from "next-intl";
 const NavBar = () => {
     const t = useTranslations("home.navbar");
 
-   return (
-       <div className={'flex p-5 text-white items-center bg-gradient-to-b from-gradient-from'}>
+    return (
+        <div className={'flex p-5 text-white items-center bg-white bg-opacity-25'}>
             <div className={'flex items-center'}>
                 <Image src='/assets/images/logo.png' alt={'Logo'} width={100} height={100} />
                 <div className={'flex flex-col font-bold text-xl -space-y-1 tracking-wider'}>
@@ -15,18 +15,18 @@ const NavBar = () => {
                 </div>
             </div>
 
-           <div className={'flex justify-between w-3/5 ml-14 text-sm font-bold '}>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Home')}</h1>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('About Us')}</h1>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Students')}</h1>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Admission')}</h1>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Academics')}</h1>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Research')}</h1>
-               <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Resources')}</h1>
-           </div>
+            <div className={'flex justify-between w-3/5 ml-14 text-sm font-bold '}>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Home')}</h1>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('About Us')}</h1>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Students')}</h1>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Admission')}</h1>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Academics')}</h1>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Research')}</h1>
+                <h1 className={'uppercase cursor-pointer hover:underline decoration-primary decoration-4 duration-300 underline-offset-4'}>{t('Resources')}</h1>
+            </div>
 
-       </div>
-   )
+        </div>
+    )
 }
 
 export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,32 @@ import Heading from "../components/HomePageComponents/Heading";
 import {motion} from "framer-motion";
 import ScrollHint from "../components/HomePageComponents/ScrollHint";
 import Button from "../components/HomePageComponents/Button";
+import Cards from "../components/HomePageComponents/Cards";
+
+import Image from "next/image";
 
 
 const Home = () => {
     return (
-        <motion.div animate={{
-            backgroundColor:['#000', 'rgba(0,0,0,0)']
-        }} className={'h-screen -z-10 flex flex-col'}>
-            <div>
-                <video autoPlay muted loop className={'fixed w-full h-screen brightness-50 -z-50 object-fill'}>
-                    <source src="/assets/video/bg.mp4" type="video/mp4"/>
-                </video>
-            </div>
-            <NavBar/>
-            <Heading/>
-            <Button/>
-            <ScrollHint/>
-        </motion.div>
+        <div>
+            <motion.div animate={{
+                backgroundColor:['#000', 'rgba(0,0,0,0)']
+            }} className={'h-screen -z-10 flex flex-col'}>
+
+                <div>
+                    <video autoPlay muted loop className={'fixed w-full h-screen brightness-50 -z-50 object-fill'}>
+                        <source src="/assets/video/bg.mp4" type="video/mp4"/>
+                    </video>
+                    <NavBar/>
+                    <Heading/>
+                    <Button/>
+                    <ScrollHint/>
+                    <Cards/>
+                </div>
+            </motion.div>
+
+        </div>
+
     )
 }
 
