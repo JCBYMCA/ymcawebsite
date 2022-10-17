cf43173f6340bc8412415fe281f4bcf14dad208b
Mister-H <hs913271@gmail.com>
2022-10-17 13:36:19 +0530

    tagline

---
+++ config/LandingPageCardsData.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        title: "About",
+    },
+    {
+        title: "About",
+    },
+    {
+        title: "About",
+    },
+    {
+        title: "About",
+    }
+]
