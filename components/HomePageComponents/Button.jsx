ee97bb38895d849ed01d7333ed95f20944de1c79
srv332003 <srvgarg332003@gmail.com>
2023-02-09 14:15:22 +0530

    fixed some homepage responsiveness

--- components/HomePageComponents/Button.jsx
+++ components/HomePageComponents/Button.jsx
@@ -5,10 +5,10 @@ const Button = () => {
     const t = useTranslations("home.button");
 
     return(
-        <motion.div animate={{opacity:[0,1]}} transition={{delay:0.8}} className="flex mt-16 md:ml-20 space-x-4 md:space-x-11">
-            <button className="uppercase duration-300 text-white border-2 p-1 md:p-4 text-xs md:text-sm hover:text-primary hover:bg-white hover:border-primary font-bold">{t('Find a Program')}</button>
-            <button className="uppercase duration-300 text-white border-2 p-1 md:p-4 text-xs md:text-sm hover:text-primary hover:bg-white hover:border-primary font-bold">{t('Request Information')}</button>
-            <button className="uppercase duration-300 text-white border-2 p-1 md:p-4 text-xs md:text-sm hover:text-primary hover:bg-white hover:border-primary font-bold">{t('Apply Now')}</button>
+        <motion.div animate={{opacity:[0,1]}} transition={{delay:0.8}} className="flex mt-3 md:mt-7  md:ml-20 space-x-4 md:space-x-11">
+            <button className="uppercase duration-300 text-white border-2 p-1 md:p-4 text-[0.5rem] md:text-sm hover:text-primary hover:bg-white hover:border-primary font-bold">{t('Find a Program')}</button>
+            <button className="uppercase duration-300 text-white border-2 p-1 md:p-4 text-[0.5rem] md:text-sm hover:text-primary hover:bg-white hover:border-primary font-bold">{t('Request Information')}</button>
+            <button className="uppercase duration-300 text-white border-2 p-1 md:p-4 text-[0.5rem] md:text-sm hover:text-primary hover:bg-white hover:border-primary font-bold">{t('Apply Now')}</button>
         </motion.div>
     )
 }
