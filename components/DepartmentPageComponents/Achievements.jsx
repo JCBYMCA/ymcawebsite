d90bf3336e67891304b5225b33ef75017ea85ffe
chirag-unision <sharmacj123@gmail.com>
2024-01-24 16:43:00 +0530

    a minor error fix

--- components/DepartmentPageComponents/Achievements.jsx
+++ components/DepartmentPageComponents/Achievements.jsx
@@ -84,15 +84,15 @@ const Achievements = ({id}) => {
                     dotListClass="custom-dot-list-style"
                     itemClass="carousel-item-padding-40-px"
                 >
-                    {achievement && achievement.map((item)=>{
-                        return <Achievement text={item.description} image_src={'http://49.50.77.87:8000/media/'+item.img}/>
+                    {achievement && achievement.map((item, i)=>{
+                        return <Achievement key={i} text={item.description} image_src={'http://49.50.77.87:8000/media/'+item.img}/>
                     })}
-                    <Achievement text={'Dr. Neelam Duhan has been awarded the AICTE Visvesvaraya Best Teacher Award 2021. The award carries a cash prize of Rs 25,000 with a medal and certificate of excellence.'} image_src={'/assets/images/achievements/1.png'}/>
+                    {/* <Achievement text={'Dr. Neelam Duhan has been awarded the AICTE Visvesvaraya Best Teacher Award 2021. The award carries a cash prize of Rs 25,000 with a medal and certificate of excellence.'} image_src={'/assets/images/achievements/1.png'}/>
                     <Achievement text={'The officials of university have been acknowledged for their support and contribution during the lockdown period of Covid-19 Pandemic from March to May, 2020, by the Haryana Red Cross Society.'} image_src={'/assets/images/achievements/2.png'}/>
                     <Achievement text={'The team of women innovators of the University was recently awarded the Lilavati Award 2021-22 on Women Empowerment by AICTE. The award carries a cash prize of Rs. 75,000 and an appreciation certification.'} image_src={'/assets/images/achievements/3.png'}/>
                     <Achievement image_src={'/assets/images/achievements/4.png'} text={'Dr. Sushil Kumar Panwar won second prize in the three-day State Level YRC Orientation Workshop for YRC University Programme Coordinators and Counsellors on YRC Guidelines and Activities held from August 26 to 28,2022.'}/>
                     <Achievement image_src={'/assets/images/achievements/5.png'} text={'The University & The University Computer Centre had set-up &apos;COVID-19 Help-Desk&apos; to provide a Common Resource Platform for COVID patients with a dedicated team of over 200 student volunteers to assist critically ill patients by arranging medical facilities.'}/>
-                    <Achievement image_src={'/assets/images/achievements/6.png'} text={'The university has been awarded the &apos;Youth Red Cross Shield&apos;, &apos;Youth Red Cross Award&apos;, &apos;Voluntary Blood Donation Award&apos;, and a &apos;Souvenir&apos; by the Haryana State Branch of Indian Red Cross Society (IRCS) in recognition of the University&apos;s outstanding contribution in voluntary blood donation.'}/>
+                    <Achievement image_src={'/assets/images/achievements/6.png'} text={'The university has been awarded the &apos;Youth Red Cross Shield&apos;, &apos;Youth Red Cross Award&apos;, &apos;Voluntary Blood Donation Award&apos;, and a &apos;Souvenir&apos; by the Haryana State Branch of Indian Red Cross Society (IRCS) in recognition of the University&apos;s outstanding contribution in voluntary blood donation.'}/> */}
                 </Carousel>
             </div>
         </div>
