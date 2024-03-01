255617e19c749b9802cc48f6bc436f210782606d
chirag-unision <sharmacj123@gmail.com>
2024-03-01 16:47:59 +0530

    notice issue fixed

--- pages/department/[...slug].jsx
+++ pages/department/[...slug].jsx
@@ -63,7 +63,7 @@ const DepartmentPage = ({ slug, setLoader }) => {
                 </div>
                 <div className={'flex flex-col md:flex-row bg-notice-bg bg-cover bg-opacity-5 w-full md:h-[35.8rem] py-10 px-3 md:px-8'}>
                     <div className={'md:w-2/3 w-auto shadow-sm md:mr-4'}>
-                        <Notices className={'bg-[#EBEBEB] border-solid'} isDepartment={true}  isHome={false} />
+                        <Notices className={'bg-[#EBEBEB] border-solid'} isDepartment={true}  isHome={false} deptID={departmentId[slug[0]]} />
                     </div>
                     <div className={'md:w-1/3 w-auto md:ml-4 mt-4 md:mt-0'}>
                         <QuickLinks heading={'Scheme & Syllabus'} className={'bg-[#EBEBEB] border-solid'}/>
