0a460f9c063906d3dc2b6224e3fa952b2b5e68b4
Sourav <srvgarg332003@gmail.com>
2024-03-11 13:00:31 +0530

    homepage video content settled

--- components/HomePageComponents/Heading.jsx
+++ components/HomePageComponents/Heading.jsx
@@ -7,7 +7,7 @@ const Heading = () => {
     return (
         <div className={"text-white overflow-hidden"}>
 
-            <motion.div animate={{opacity:[0,1]}} transition={{delay:0.8}} className={'w-full'}>
+            <motion.div animate={{opacity:[0,1]}} transition={{delay:0.8}} className={' z-10 relative w-full'}>
                 <h1 className={'ml-0 mt-24 md:mt-48 md:w-[50rem] w-[20rem] pl-10 text-sm md:leading-[5rem] z-10 relative font-bold md:text-5xl '}>{t("tagline")}</h1>
                 <h1 className={'text-xs md:text-xl md:ml-[35rem] md:mt-0 ml-[6rem] italic w-[14rem] md:w-[16rem]'}>~ {t('jcbose')}</h1>
             </motion.div>
